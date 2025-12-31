60b0133f085cbeaf33adbfb5bb10552a1e457106
Karo <sylvain.brunet37@gmail.com>
2025-12-31 14:48:09 -0500

    feat(ex1): created Potato actor

---
+++ Source/PotatoGame/Crops/NutritionalInformations.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NutritionalInformations.generated.h"
+
+USTRUCT()
+struct FNutritionalInformations
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere)
+	float Calories;
+	
+	UPROPERTY(EditAnywhere)
+	float Carbs;
+	
+	UPROPERTY(EditAnywhere)
+	float Fat;
+	
+	UPROPERTY(EditAnywhere)
+	float Proteins;
+};
---
+++ Source/PotatoGame/Crops/Potato.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NutritionalInformations.h"
+#include "Potato.generated.h"
+
+UCLASS()
+class APotato : public AActor
+{
+public:
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditAnywhere)
+	float Weight;
+	
+	UPROPERTY(EditAnywhere)
+	FNutritionalInformations NutritionalInformations;
+};
